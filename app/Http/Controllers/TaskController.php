eece9f95f83b4ba9e839d0df87b86b5a78cc37db
Feltimos <johnfeltimosmichael@gmail.com>
2024-12-04 16:51:12 +0800

    first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+        ]);
+
+        Task::create($request->all());
+        return redirect()->route('tasks.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Task $task)
+    {
+        $request->validate([
+            'title' => 'required|string',
+            'description' => 'required|string',
+            'completed' => 'boolean',
+        ]);
+
+        $task->update($request->all());
+        return redirect()->route('tasks.index');
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/Home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Task Scheduler</title>
+</head>
+<body>
+    <h1>Welcome to Task Scheduler</h1>
+    <a href="{{ route('tasks.index') }}">View Tasks</a>
+</body>
+</html>
